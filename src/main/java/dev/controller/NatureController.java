fe62d9a57feb95c1e07790cb1b63453a15d999fc
Jonathan Tapin <jonathan@tapin.com>
2020-05-11 14:44:37 +0200

    USGMD015 - #15 6 : nature controller

--- src/main/java/dev/controller/NatureController.java
+++ src/main/java/dev/controller/NatureController.java
@@ -1,9 +1,13 @@
 package dev.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +26,23 @@ public class NatureController {
 	public List<Nature> natures() {
 		List<Nature> natures = natureRepo.findAll();
 		return natures;
+	}
+	
+	/** PATCH :  mis à jour d'une nature de mission **/
+	
+	@PatchMapping
+	@RequestMapping(value="/modification")
+	public ResponseEntity<String> updateClient(@RequestBody Nature nature){
+		Nature currentNature = natureRepo.findById(nature.getId());
+			currentNature.setNom(nature.getNom());
+			currentNature.setPrime(nature.isPrime());
+			currentNature.setFacturation(nature.isFacturation());
+			currentNature.setTjm(nature.getTjm());
+			currentNature.setPourcentage(nature.getPourcentage());
+			currentNature.setPlafond(nature.getPlafond());
+			currentNature.setPlafond_depassable(nature.isPlafond_depassable());
+				
+		return
 	}
 
 }
--- src/main/java/dev/repository/NatureRepo.java
+++ src/main/java/dev/repository/NatureRepo.java
@@ -10,4 +10,6 @@ public interface NatureRepo extends JpaRepository<Nature, Long> {
 
 	Optional<Nature> findByNom(String nature);
 
+	Nature findById(Integer id);
+
 }
