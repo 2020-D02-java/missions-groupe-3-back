36db8a46caa973cd87ece7f8089e905fad345611
antoinethebault <antoine.thebau@gmail.com>
2020-05-18 15:15:18 +0200

    gestion de suppression de mission avec ligne de frais et note de frais associes

--- src/main/java/dev/repository/LigneDeFraisRepo.java
+++ src/main/java/dev/repository/LigneDeFraisRepo.java
@@ -1,9 +1,13 @@
 package dev.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.domain.LigneDeFrais;
 
 public interface LigneDeFraisRepo extends JpaRepository<LigneDeFrais, Long> {
+
+	List<LigneDeFrais> findByNoteFraisID(Integer id);
 
 }
